d170dee5b56d25b766642986edfc7b24993d08c2
Juliana Dávila R <gobolindo@gmail.com>
2012-12-21 15:10:19 -0500

    cloned spree from master and executed copy of migrations, initializers and generate sample data with 'rails g spree:install'
    Signed-off-by: Juliana Dávila R <gobolindo@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoStore::Application.config.secret_token = 'b1fc20dd83c0edbde9152d47b52dbd5ca833748eca80ae0a38314e3fa0884d7c70f87b093b3ccc897257dfe968113d5cec2ac27819401979d5cacf62132f12a7'
